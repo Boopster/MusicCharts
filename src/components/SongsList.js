cba35f24e94363bf04cfbdd9209bbf43610575c8
Rebecca Still <beckystill@hotmail.com>
2019-11-25 20:04:25 +0000

    add SongsList component

---
+++ src/components/SongsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SongsList = (props) => {
+  return (
+    <div>
+      <ul>
+        <li>Songs List</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SongsList;
--- src/containers/MusicChart.js
+++ src/containers/MusicChart.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
+import SongsList from '../components/SongsList'
 
 class MusicChart extends Component {
   constructor(props){
     super(props);
     this.state={
-      songs: [],
+      songsList: [],
     }
   }
+
   componentDidMount(){
     const url='http://itunes.apple.com/gb/rss/topsongs/limit=20/json'
     fetch(url)
     .then(res => res.json())
-    .then(data => this.setState({
-      songs: data
+    .then(songsData => this.setState({
+      songsList: songsData
     }))
     .catch(err => console.error());
   }
+
   render() {
     return(
-      <h1>iTunes Store: Top Songs</h1>
+      <div>
+        <h1>iTunes Store: Top Songs</h1>
+        <SongsList songsList={this.state.songsList}/>
+      </div>
     )
   }
 }
