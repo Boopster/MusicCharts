3e0a0fa0c05275b3d8637761c0034f531cc9604b
Rebecca Still <beckystill@hotmail.com>
2019-11-25 22:12:41 +0000

    refactor SongsList component

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const SongsList = (props) => {
-
+  const topSongs = props.songsList.map((song, index) => {
+    return <li>Chart Position: {index+1} {song.title.label}</li>
+  })
   return (
     <div>
       <ul>
-        <li>Top Songs</li>
+        {topSongs}
       </ul>
     </div>
   )
