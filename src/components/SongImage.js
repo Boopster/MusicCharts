e6c9728267917bac356e78b56d70d3bbd4e76f91
Rebecca Still <beckystill@hotmail.com>
2019-11-26 09:04:25 +0000

    fix syntax for image url

--- src/components/SongImage.js
+++
@@ -1,1 +0,0 @@
-<img src={url} alt="" height="100" width="100"></img>
--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -3,7 +3,7 @@ import './SongsList.css';
 
 const SongsList = (props) => {
   const topSongs = props.songsList.map((song, index) => {
-    const url = song["im:image"][0].attributes.label;
+    const url = song["im:image"][0].label;
     return (
         <div className="main-container">
         <img src={url} alt="" height="80" width="80"></img>
